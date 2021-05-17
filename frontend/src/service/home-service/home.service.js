5e62cdd9feb7237aa32daf805c7e193c896d0417
Amnesiye <amnesiye.dev@gmail.com>
2021-05-17 19:19:06 +0200

    rename HomeService

--- frontend/src/service/home-service/home.service.js
+++ frontend/src/service/home-service/home.service.js
@@ -1,33 +1,33 @@
 import http from "../http-common";
 
-class TutorialDataService {
+class HomeService {
   getAll() {
     return http.get("/tests");
   }
 
   /*get(id) {
-    return http.get(`/tutorials/${id}`);
+    return http.get(`/tests/${id}`);
   }
 
   create(data) {
-    return http.post("/tutorials", data);
+    return http.post("/tests", data);
   }
 
   update(id, data) {
-    return http.put(`/tutorials/${id}`, data);
+    return http.put(`/tests/${id}`, data);
   }
 
   delete(id) {
-    return http.delete(`/tutorials/${id}`);
+    return http.delete(`/tests/${id}`);
   }
 
   deleteAll() {
-    return http.delete(`/tutorials`);
+    return http.delete(`/tests`);
   }
 
   findByTitle(title) {
-    return http.get(`/tutorials?title=${title}`);
+    return http.get(`/tests?title=${title}`);
   }*/
 }
 
-export default new TutorialDataService();
+export default new HomeService();
