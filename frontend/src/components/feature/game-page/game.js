b5844f107537c1e55c6d083328755bad8a4c16ce
Quentin Vignan <quentinvignan@MacBook-Pro-de-Quentin.local>
2021-08-14 15:12:45 +0200

    add client and server class

--- frontend/src/components/feature/game-page/game.js
+++ frontend/src/components/feature/game-page/game.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "../../../config/app.url.json";
-import Uno from "../../../games/uno/uno";
 import "./game.scss";
 
 function Game() {
   return (
     <div className="game-scene">
-      <Uno />
     </div>
   );
 }
