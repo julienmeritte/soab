497c2dce176e88107a540040661358d32e218bc4
Amnesiye <amnesiye.dev@gmail.com>
2021-08-19 21:29:41 +0200

    rebase

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React, {Fragment, useState, useEffect, Suspense, lazy} from "react";
 import "./App.scss";
-import Axios from "axios";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomePage from "./components/feature/home-page/HomePage";
 import Navbar from "./components/core/header/Navbar";
