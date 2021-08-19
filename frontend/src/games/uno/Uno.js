56518ddc682f577b7e043771eec7f2ce5cf2f5f7
Amnesiye <amnesiye.dev@gmail.com>
2021-08-19 23:06:43 +0200

    fix superposition et stopPropagation cartes

--- frontend/src/games/uno/Uno.js
+++ frontend/src/games/uno/Uno.js
@@ -1,14 +1,23 @@
 import Card from "../components/Card";
 import Board from "../components/Board";
-import {Canvas} from "@react-three/fiber";
+import {Canvas, useThree} from "@react-three/fiber";
 import React, { useState } from "react";
 import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader";
 import deck from "./deck.json";
 import properties from "./properties.json";
+import {useGesture} from "@use-gesture/react";
+
+import * as THREE from "three";
+import {Vector3} from "three";
 
 export default function Uno() {
 
     const loader = new GLTFLoader();
+
+    const camera = {fov: 75, position: [0, 0, 65]}
+
+    let vec = new THREE.Vector3();
+    let pos = new THREE.Vector3();
 
     const cards = [];
 
@@ -19,7 +28,7 @@ export default function Uno() {
     }
 
     return (
-        <Canvas camera={{fov: 75, position: [0, 0, 65]}} resize={{scroll: false, debounce: {scroll: 0, resize: 0}}}>
+        <Canvas camera={camera} resize={{scroll: false, debounce: {scroll: 0, resize: 0}}}>
             <ambientLight intensity={0.5}/>
             <spotLight position={[10, 15, 10]} angle="0.3"/>
             <Board/>
@@ -29,9 +38,5 @@ export default function Uno() {
 
     function cardOnClick(index) {
         cards[index].props.position[1] += 10;
-    }
-
-    function detectSuperposition(position) {
-
     }
 }
