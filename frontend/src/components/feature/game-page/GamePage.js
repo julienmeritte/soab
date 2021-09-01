2dcebabaa3aefa65326c4825326ace0de95a199a
Amnesiye <amnesiye.dev@gmail.com>
2021-09-01 16:33:18 +0200

    ajout bouton prêt + lancement quand allReady

--- frontend/src/components/feature/game-page/GamePage.js
+++ frontend/src/components/feature/game-page/GamePage.js
@@ -5,6 +5,8 @@ import "./GamePage.scss";
 import openSocket from 'socket.io-client';
 import Uno from "../../../games/uno/Uno";
 import {GAMES_ENUM} from "../../../enums/games-enum";
+import Card from "../../../games/components/Card";
+import properties from "../../../games/uno/properties.json";
 
 const socket = openSocket('http://localhost:3002');
 
@@ -13,17 +15,11 @@ class GamePage extends React.Component {
     constructor(props) {
         super(props)
 
-        this.currentGame = [
-            <div className={'game-scene'}>
-                <Uno/>
-            </div>
-        ];
-
         this.state = {
             roomCreated: false,
             player: {},
             game: GAMES_ENUM.UNO,
-            allReady: true,
+            allReady: false,
             listPlayer: [],
             name: "",
             room: "",
@@ -33,15 +29,28 @@ class GamePage extends React.Component {
     componentDidMount() {
         setInterval(() => {
             if (this.state.roomCreated) {
-                socket.emit('getAllPlayersByRoom', {room: this.state.player.room});
-                socket.on('givePlayersByRoom', (value) => {
-                    this.setState({
-                        listPlayer: value,
-                    })
+                socket.emit('getAllPlayersByRoom', {
+                    room: this.state.player.room
                 });
+                socket.on('givePlayersByRoom', (data) => {
+                    this.setState({
+                        listPlayer: data.players,
+                    });
+                });
+                var ready = true;
+                for(const player of this.state.listPlayer) {
+                    if (player.ready === false) {
+                        ready = false;
+                    }
+                }
+                if (!this.state.allReady && this.state.listPlayer.length > 0 && ready) {
+                    this.setState({
+                        allReady: true
+                    });
+                }
             }
-            console.log('player: ', this.state.player);
-            console.log('listPlayer: ', this.state.listPlayer);
+            /*console.log('listPlayer: ', this.state.listPlayer);
+            console.log('player: ', this.state.player);*/
         }, 1000);
     }
 
@@ -59,13 +68,23 @@ class GamePage extends React.Component {
         this.setState({
             roomCreated: true,
         })
-        socket.emit('createPlayer', {name: this.state.name, room: this.state.room});
+        socket.emit('createPlayer', {
+            name: this.state.name,
+            room: this.state.room
+        });
         socket.on('getPlayer', (value) => {
             this.setState({
                 player: value,
             })
         })
     };
+
+    setPlayerReady = () => {
+        console.log('THIS', this.state.player.code);
+        socket.emit('setPlayerReady', {
+            code: this.state.player.code
+        });
+    }
 
     render() {
         const {roomCreated, allReady, name, room, player} = this.state;
@@ -81,11 +100,22 @@ class GamePage extends React.Component {
                     ) : (
                         <div/>
                     )}
-                    <div>
-                    </div>
-                    {allReady ? this.currentGame : (
-                        <div/>
-                    )}
+
+                    {allReady ? (
+                            <div className={'game-scene'}>
+                                <Uno/>
+                            </div>
+                        )
+                        : (
+                            <div>
+                                <button className={'game-button-ready'} onClick={this.setPlayerReady}>Prêt</button>
+                                {this.state.listPlayer.map((player) => <div>
+                                        {player.name} : {player.ready ? (<span>Prêt</span>) : (<span>Pas prêt</span>)}
+                                    </div>
+                                )}<br/>
+                                Tous les joueurs ne sont pas prêts.
+                            </div>
+                        )}
                 </div>
             </div>
         )
