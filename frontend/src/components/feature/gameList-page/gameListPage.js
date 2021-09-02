45dec7b27bc795a5c1007938e82c1507d41f8a0c
ladeir_j <jeremy.ladeira@olyrix.com>
2021-09-03 00:49:23 +0200

    Added a part of the game database

--- frontend/src/components/feature/gameList-page/gameListPage.js
+++ frontend/src/components/feature/gameList-page/gameListPage.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "./gameListPage.scss";
 import {GAMES_ENUM} from "../../../enums/games-enum";
-import { Redirect } from 'react-router'
+import { Redirect } from 'react-router';
+import { loadGames } from "../../../actions/game";
 
 
 class GameListPage extends React.Component {
     
-    
-
     constructor(props) {
         super(props)
         this.state = {
@@ -15,11 +14,13 @@ class GameListPage extends React.Component {
             redirect: false,
         }
     }
-
+    
     componentDidMount() {
+        console.log('SHEEEEEEEEEEEESH');
+        loadGames();
         let arr = [];
         for (let index = 0; index < 10; index++) {
-            arr.push({name : "UNO" , value : GAMES_ENUM.UNO , descr: "je suis la description" , key : index});
+            arr.push({name : "UNO" , value : GAMES_ENUM.UNO , descr: "Un simple jeu de carte connu de tous" ,image_source: "./assets/images/games/uno_background.png" , key : index});
         }
         this.setState({
             ListGame : arr
@@ -44,12 +45,18 @@ class GameListPage extends React.Component {
             <div>
                 {ListGame.map((value, index) => {
                     return (
-                        <div key={value.key}>
-                            <h1>{value.name}</h1>
-                            <p>{value.descr}</p>
-                            <button type="primary" onClick={() => this.handleGameSelect(value.value)}>
-                                Jouer
-                            </button>
+                        <div class="games" key={value.key}>
+                            <ul>
+                                <li>    
+                                    <div class="game-item">
+                                        <h1>{value.name}</h1>
+                                        <a onClick={() => this.handleGameSelect(value.value)}>
+                                            <img src={value.image_source}></img>
+                                            <p class="desc">{value.descr}</p>
+                                        </a>
+                                    </div>
+                                </li>
+                            </ul>
                         </div>
                     )
                 })}
